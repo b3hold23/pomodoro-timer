3f81de625bbee6b87f36cffb029548f3dfccefe3
unknown <rosa.angel.daniel@gmail.com>
2025-04-09 18:45:28 -0600

    full pomodoro timer works

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 const timer = document.getElementById('timer');
+const timerValues = document.getElementById('timerValues');
 const smTimer = document.getElementById('smTimer');
 const mdTimer = document.getElementById('mdTimer');
 const lgTimer = document.getElementById('lgTimer');
@@ -6,8 +7,10 @@ const xlTimer = document.getElementById('xlTimer');
 
 
 let countDown;
+let timerLength = 0;
 let isRunning = false;
 let workSession = true;
+let breakTime = false;
 
 function startTimer(durationInMin, timeDown) {
     clearInterval(countDown);
@@ -31,21 +34,58 @@ function startTimer(durationInMin, timeDown) {
             if (timeLeft <= 0) {
                 clearInterval(countDown);
                 isRunning = false;
+               
+                let breakLength = 0;
+
+                if (timerLength === 25) {
+                    breakLength = 5;
+                } else if (timerLength === 30) {
+                    breakLength = 10;
+                } else if (timerLength === 45) {
+                    breakLength = 15;
+                } else if (timerLength === 60) {
+                    breakLength = 20;
+                }
+
+                const breakBtn = document.createElement('button');
+                breakBtn.textContent = "Start Break";
+
+                timer.appendChild(breakBtn);
+
+                breakBtn.addEventListener('click', () => {
+                    breakBtn.remove();
+                    timer.innerHTML = "";
+                    const breakTimer = document.createElement('h1');
+                    breakTimer.textContent = `${String(breakLength).padStart(2, '0')}:00`;
+                
+                    timer.appendChild(breakTimer);
+                
+                    workSession = false;
+                    breakTime = true;
+                    timerLength = breakLength;
+                
+                    startTimer(breakLength, breakTimer);
+                
+                });
             }
         }, 1000)
 }
+
     
 smTimer.addEventListener('click', () => {
+    timerValues.remove();
     let timeDown = document.createElement('h1');
     timeDown.textContent = "25:00";
     timer.appendChild(timeDown);
 
     let time = 25;
+    timerLength = time;
 
-    startTimer(time, timeDown);
+    startTimer(time, timeDown,);
 })
 
 mdTimer.addEventListener('click', () => {
+    timerValues.remove();
     let timeDown = document.createElement('h1');
     timeDown.textContent = "30:00";
     timer.appendChild(timeDown);
@@ -56,6 +96,7 @@ mdTimer.addEventListener('click', () => {
 })
 
 lgTimer.addEventListener('click', () => {
+    timerValues.remove();
     let timeDown = document.createElement('h1');
     timeDown.textContent = "45:00";
     timer.appendChild(timeDown);
@@ -66,6 +107,7 @@ lgTimer.addEventListener('click', () => {
 })
 
 xlTimer.addEventListener('click', () => {
+    timerValues.remove();
     let timeDown = document.createElement('h1');
     timeDown.textContent = "60:00";
     timer.appendChild(timeDown);
